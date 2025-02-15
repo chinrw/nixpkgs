4ffadc2b6f0ee629a5cc99b667b83109f16ff4c5
Ruowen Qin <chinqrw@gmail.com>
2025-02-15 20:50:48 +0800

    cc-wrapper: remove -nostdlibinc
    Signed-off-by: Ruowen Qin <chinqrw@gmail.com>

--- pkgs/build-support/cc-wrapper/default.nix
+++ pkgs/build-support/cc-wrapper/default.nix
@@ -822,18 +822,18 @@ stdenvNoCC.mkDerivation {
     # breaks `-isysroot` with an unwrapped compiler. As macOS has
     # no `/usr/include`, there’s essentially no risk to dropping
     # the flag there. See discussion in NixOS/nixpkgs#191152.
-    #
-    +
-      optionalString
-        (
-          (cc.isClang or false)
-          && !(cc.isROCm or false)
-          && !targetPlatform.isDarwin
-          && !targetPlatform.isAndroid
-        )
-        ''
-          echo "-nostdlibinc" >> $out/nix-support/cc-cflags
-        ''
+    # Remove this for kernel compile
+    # +
+      # optionalString
+      #   (
+      #     (cc.isClang or false)
+      #     && !(cc.isROCm or false)
+      #     && !targetPlatform.isDarwin
+      #     && !targetPlatform.isAndroid
+      #   )
+      #   ''
+      #     echo "-nostdlibinc" >> $out/nix-support/cc-cflags
+      #   ''
 
     ##
     ## Man page and info support
